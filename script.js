e5a530d5a67d2187c2fc5609be02cbbdac6f1fdf
Pratiksha Chikane <92205488+pratiksha2101@users.noreply.github.com>
2025-04-11 14:16:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Future enhancements can go here
+console.log("Portfolio Loaded");
+document.getElementById('darkModeToggle').addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
